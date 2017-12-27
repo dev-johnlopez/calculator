b07282379f54fd3eea60366b6edcef5d421851a5
John Lopez <johnny.lopez617@gmail.com>
2017-12-27 11:06:57 -0600

    updates

--- app/web/controllers/deal.py
+++ app/web/controllers/deal.py
@@ -16,8 +16,6 @@ def create():
     if form.validate_on_submit():
         createDealFromForm(form)
         return redirect(url_for('dashboard.index'))
-    else:
-        flash('%s' % current_user)
     form.units.append_entry(UnitForm())
     form.units.append_entry(UnitForm())
     form.units.append_entry(UnitForm())
